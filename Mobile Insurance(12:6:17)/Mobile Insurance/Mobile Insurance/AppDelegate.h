99fac6820629c79087f7b2087b60acc64bc0a7c5
NAVEENRAJ3692 <nraj030692@gmail.com>
2017-06-12 19:49:00 +0530

    Add files via upload

---
+++ Mobile Insurance(12:6:17)/Mobile Insurance/Mobile Insurance/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Mobile Insurance
+//
+//  Created by Apple on 12/06/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Mobile Insurance(12:6:17)/Mobile Insurance/Mobile Insurance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mobile Insurance
+//
+//  Created by Apple on 12/06/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
